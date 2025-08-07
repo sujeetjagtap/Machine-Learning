7e946264411f0a81443012a36c958595f82d1a20
sujeetjagtap <101381786+sujeetjagtap@users.noreply.github.com>
2025-08-07 09:10:36 +0530

    Python3 requirements for ML.

---
+++ 1 Linear Algebra/requirements.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# All required dependencies for Machine Learning Lab v1.0
+
+joblib
+numpy
+scipy
+sympy
+pandas
+scikit-learn
+category_encoders
+#hashlib
+matplotlib
+seaborn
+tensorflow
+#tensorflow-protobuf
+qiskit
+qiskit_machine_learning
+torch
+torchvision
+notebook
+spyder
